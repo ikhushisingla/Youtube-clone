b1128633528db2c7fc937438a310359efb5bc349
ikhushisingla <singlakhushi822@gmail.com>
2023-09-30 22:22:52 +0530

    videos section change done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -10,7 +10,7 @@ const Videos = ({ videos, direction }) => {
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} /> }
+          {(item.id.videoId || item.id.playlistId) && <VideoCard video={item} /> }
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}

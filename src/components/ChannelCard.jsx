a80e19b28220f9f12e616d46c8de8938eeb06da9
ikhushisingla <singlakhushi822@gmail.com>
2023-09-30 19:44:55 +0530

    width height change

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import {demoProfilePicture} from '../utils/constants'
 
 const ChannelCard = ({channelDetail}) => (
-  <Box sx={{boxShadow:'none' , borderRadius:'20px',display:'flex',justifyContent:'center',alignItems:'center',width:{xs:'356px',md:'320px'} ,height:'326px',margin:'auto'}}>
+  <Box sx={{boxShadow:'none' , borderRadius:'20px',display:'flex',justifyContent:'center',alignItems:'center',width:{xs:'356px',md:'310px'} ,height:'326px',margin:'auto'}}>
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent sx={{display:'flex',flexDirection: 'column',justifyContent:'center',textAlign:'center',color:'#fff'}}>
         <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{borderRadius:'50%',height:'180px',width:'180px',mb:2,border:'1px solid #e3e3e3'}}/>

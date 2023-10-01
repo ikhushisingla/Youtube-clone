b22b806b5600dc84fb3289bdd1ae5d808a7c3cf4
ikhushisingla <singlakhushi822@gmail.com>
2023-10-01 13:33:55 +0530

    channelDetail done

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import {demoProfilePicture} from '../utils/constants'
 
 const ChannelCard = ({channelDetail}) => (
-  <Box sx={{boxShadow:'none' , borderRadius:'10px',display:'flex',justifyContent:'center',alignItems:'center',width:{xs: '100%', sm: '358px', md: "290px"} ,height:'326px',margin:'auto'}}>
+  <Box sx={{boxShadow:'none' , borderRadius:'10px',display:'flex',justifyContent:'center',alignItems:'center',width:{xs: '100%', sm: '358px', md: "290px"} ,height:'326px',margin:'auto',marginTop:'-93px'}}>
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent sx={{display:'flex',flexDirection: 'column',justifyContent:'center',textAlign:'center',color:'#fff'}}>
         <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{borderRadius:'50%',height:'180px',width:'180px',mb:2,border:'1px solid #e3e3e3'}}/>
@@ -16,8 +16,7 @@ const ChannelCard = ({channelDetail}) => (
 
         {channelDetail?.statistics?.subscriberCount && (
           <Typography>
-            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} 
-              Subscribers
+            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers 
           </Typography>)}
       </CardContent>
     </Link>

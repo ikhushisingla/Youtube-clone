8a8e1890f9afbdb25ebc6bef3b37df739da4a10e
ikhushisingla <singlakhushi822@gmail.com>
2023-10-02 17:33:24 +0530

    changes done

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import {Box , CircularProgress,Stack} from '@mui/material'
-const Loader = () => {
-  return (
-    <Box minHeight="95vh">
-      <Stack direction="row" justifyContent="center" alignItems="center" height="80vh">
-        <CircularProgress/>
-      </Stack>
-    </Box>
-  )
-}
+import React from 'react';
+import { Box, CircularProgress, Stack } from '@mui/material';
 
-export default Loader
+const Loader = () =>  (
+  <Box minHeight="95vh">
+    <Stack direction='row' justifyContent='center' alignItems='center' height='80vh' >
+      <CircularProgress />
+    </Stack>
+  </Box>
+);
+
+export default Loader;

5113d8f134c6ae0ff6704437f3ac4b259665514e
ikhushisingla <singlakhushi822@gmail.com>
2023-10-02 17:57:57 +0530

    project complete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -21,9 +21,12 @@ const SearchBar = () => {
     <Paper
       component='form'
       onSubmit={onhandleSubmit}
+      
       sx={{
+        alignItems:'center',
         borderRadius: 20,
-        border: '1px solid #e3e3e3',
+        border: '1px solid #e1e1e1',
+        background:'black',
         pl: 2,
         boxShadow: 'none',
         mr: { sm: 5 },
@@ -35,7 +38,7 @@ const SearchBar = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+      <IconButton type='submit' sx={{ p: '10px', color: 'white' }} aria-label='search'>
         <SearchIcon />
       </IconButton>
     </Paper>
